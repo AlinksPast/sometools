26f4957f8e4f640e667e904eb8af945114cca0b5
AlinksPast <143039630+AlinksPast@users.noreply.github.com>
2023-09-14 15:42:53 -0700

    mroe
    mroe

--- dice.js
+++ dice.js
@@ -1,67 +1,61 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const diceButtons = document.querySelectorAll('.dice-button');
-    const clearButton = document.getElementById('clear-button');
-    const rollWithModifierButton = document.getElementById('roll-with-modifier-button');
+// JavaScript code for rolling dice goes here
+function rollDice(sides) {
+    return Math.floor(Math.random() * sides) + 1;
+}
 
-    diceButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const sides = parseInt(button.dataset.sides);
-            rollDie(sides);
-        });
-    });
+function highlightResult(result, isHigh) {
+    const resultColor = isHigh ? 'green' : 'red';
+    const formattedResult = `<span style="color: ${resultColor};">${result}</span>`;
+    return formattedResult;
+}
 
-    clearButton.addEventListener('click', clearResults);
-    rollWithModifierButton.addEventListener('click', rollWithModifier);
+const diceButtons = document.querySelectorAll('.dice-button');
+const resultDisplay = document.getElementById('result-display');
+const historyList = document.getElementById('history-list');
+const roll2d20Button = document.getElementById('roll-2d20-button');
 
-    function rollDie(sides) {
-        const result = Math.floor(Math.random() * sides) + 1;
-        displayResult(`d${sides}: ${result}`);
-    }
+const maxHistoryItems = 10;
 
-    function roll2d20() {
-        const result1 = Math.floor(Math.random() * 20) + 1;
-        const result2 = Math.floor(Math.random() * 20) + 1;
-        displayResult(`2d20: ${result1}, ${result2}`);
-    }
+diceButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        const sides = parseInt(button.getAttribute('data-sides'));
+        if (!isNaN(sides)) {
+            const result = rollDice(sides);
+            resultDisplay.innerHTML = `You rolled a D${sides} and got: ${result}`;
 
-    function displayResult(resultText) {
-        const resultsContainer = document.getElementById('results');
-        const rollHistoryRadioButtons = document.querySelectorAll('input[name="roll-history"]');
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `D${sides}: ${result}`;
 
-        let maxRollsToRemember = 5; // Default to 5 rolls if none are checked
-
-        for (const radioButton of rollHistoryRadioButtons) {
-            if (radioButton.checked) {
-                maxRollsToRemember = parseInt(radioButton.value);
-                break; // Exit loop once the checked radio button is found
+            if (historyList.children.length >= maxHistoryItems) {
+                historyList.removeChild(historyList.children[0]);
             }
+
+            historyList.appendChild(listItem);
+        }
+    });
+});
+
+roll2d20Button.addEventListener('click', function () {
+    const roll1 = rollDice(20);
+    const roll2 = rollDice(20);
+
+    if (!isNaN(roll1) && !isNaN(roll2)) {
+        const isRoll1High = roll1 > roll2;
+        const highRoll = isRoll1High ? roll1 : roll2;
+        const lowRoll = isRoll1High ? roll2 : roll1;
+
+        const formattedHighRoll = highlightResult(highRoll, true);
+        const formattedLowRoll = highlightResult(lowRoll, false);
+
+        resultDisplay.innerHTML = `You rolled 2 D20s: ${formattedLowRoll} (Low) and ${formattedHighRoll} (High)`;
+
+        const listItem1 = document.createElement('li');
+        listItem1.innerHTML = `2 D20s: ${lowRoll} (Low) and ${highRoll} (High)`;
+
+        if (historyList.children.length >= maxHistoryItems) {
+            historyList.removeChild(historyList.children[0]);
         }
 
-        // Remove the oldest roll results if the number of rolls exceeds maxRollsToRemember
-        while (resultsContainer.children.length >= maxRollsToRemember) {
-            resultsContainer.removeChild(resultsContainer.children[0]);
-        }
-
-        const resultElement = document.createElement('p');
-        resultElement.textContent = resultText;
-        resultsContainer.appendChild(resultElement);
-    }
-
-    function clearResults() {
-        const resultsContainer = document.getElementById('results');
-        resultsContainer.innerHTML = ''; // Clear the roll history
-    }
-
-    function rollWithModifier() {
-        const modifier = parseInt(document.getElementById('modifier').value, 10);
-
-        if (isNaN(modifier)) {
-            alert('Please enter a valid modifier.');
-            return;
-        }
-
-        const result = Math.floor(Math.random() * 20) + 1;
-        const totalResult = result + modifier;
-        displayResult(`d20 with Modifier (+${modifier}): ${totalResult}`);
+        historyList.appendChild(listItem1);
     }
 });
